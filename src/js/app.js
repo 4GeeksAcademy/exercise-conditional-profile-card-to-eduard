ad53d5b3d49d69e01e1e9e728002fbe0495e4455
Eduard-jcs <eduardjcs14@gmail.com>
2025-11-24 04:29:04 +0000

    Todo Bien

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,30 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "Eduard" : variables.name} ${
+    variables.lastName == null ? "Cardenas" : variables.name
+  }</h1>
+          <h2>${variables.role == null ? "Desarrollador" : variables.name}</h2>
+          <h3>${variables.country == null ? "Canada" : variables.country} ${
+    variables.city == null ? "Toronto" : variables.city
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://x.com/${
+              variables.twitter == null ? "Soyeduardjose" : variables.twitter
+            }">
+            <i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github == null ? "Eduard-jcs" : variables.github
+            }">
+            <i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin == null
+                ? "eduard-jose-cárdenas-santiago-259a63231/"
+                : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram == null ? "outlander.ve.co" : variables.inst
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -56,7 +72,8 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://www.yunbitsoftware.com/blog/wp-content/uploads/2020/03/364146-PB1OW0-666.jpg",
     // social media bar position (position-left or position-right)
     socialMediaPosition: "position-right",
     // social media usernames
